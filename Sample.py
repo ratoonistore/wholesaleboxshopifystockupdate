f84caa318907b14f878f6b6412900168d8c6e89c
ratoonistore <40091416+ratoonistore@users.noreply.github.com>
2018-06-09 21:46:27 +0530

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -1,12 +1,9 @@
-import shopify
-
-shop_url = "https://5ecbc3ae838d1b44a6c4f16037a1b6f0:4d13f266b2bc45733187cf4b7001d760@ratoon-istore.myshopify.com/admin"
-
-shopify.ShopifyResource.set_site(shop_url)
-shop = shopify.Shop.current()
-
-product = shopify.Product.find(430577647646)
-for variant in product.variants:
-    variant.inventory_quantity = 100
-    variant.save()
-print "Done"
+import smtplib
+ 
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login("ratoonistore@gmail.com", "RatooniStore@2017")
+ 
+msg = "YOUR MESSAGE!"
+server.sendmail("ratoonistore@gmail.com", "ratoonistore@gmail.com", msg)
+server.quit()
