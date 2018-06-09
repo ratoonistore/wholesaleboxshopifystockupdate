e29baa57661d1fb64d66005ae389a6ddf747b2df
ratoonistore <40091416+ratoonistore@users.noreply.github.com>
2018-06-09 21:00:00 +0530

    Update Sample.py

--- Sample.py
+++ Sample.py
@@ -1,8 +1,12 @@
-#import libraries
-import shopify, requests, json, math, time, os, smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
-from datetime import datetime
+import shopify
 
-while True:
- print "test"
+shop_url = "https://5ecbc3ae838d1b44a6c4f16037a1b6f0:4d13f266b2bc45733187cf4b7001d760@ratoon-istore.myshopify.com/admin"
+
+shopify.ShopifyResource.set_site(shop_url)
+shop = shopify.Shop.current()
+
+product = shopify.Product.find(430577647646)
+for variant in product.variants:
+    variant.inventory_quantity = 100
+    variant.save()
+print "Done"
